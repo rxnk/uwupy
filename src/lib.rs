3450892d18b3e15cd35e7cab2596d0c338b03db7
igna <igna@riseup.net>
2023-03-24 23:25:42 -0300

    document and recode yes yes

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,28 @@
-#![allow(unused)]
+use pyo3::prelude::*;
+use uwuifier::uwuify_str_sse;
+
+/// uwuify a given string
+/// 
+/// this func takes a string as input and returns a uwufied input
+/// it uses "uwuify" lib (https://github.com/Daniel-Liu-c0deb0t/uwu).
+#[pyfunction]
+fn uwuify_str(text: &str) -> PyResult<String> {
+    Ok(uwuify_str_sse(text))
+}
+
+/// a IPython module to provide binding to the lib
+/// 
+/// this mod uwuify_str func to Python, allowing users to
+/// uwuify from their code.
+#[pymodule]
+fn uwupy(_py: Python, m: &PyModule) -> PyResult<()> {
+    // add uwuify_str func to our module
+    m.add_function(wrap_pyfunction!(uwuify_str, m)?)?;
+
+    // return success of uwuify
+    Ok(())
+}
+
 fn main() {
-    use pyo3::prelude::*;
-    use uwuifier::uwuify_str_sse;
-
-    /// Uwuify a string
-    #[pyfunction]
-    fn uwuify_str(text: &str) -> PyResult<String> {
-        Ok(uwuify_str_sse(text))
-    }
-
-    /// A Python binding around the library https://github.com/Daniel-Liu-c0deb0t/uwu
-    #[pymodule]
-    fn uwupy(_py: Python, m: &PyModule) -> PyResult<()> {
-        m.add_function(wrap_pyfunction!(uwuify_str, m)?)?;
-
-        Ok(())
-    }
+    // this isn't used, tho we leave as this due to "unused" warning
 }
